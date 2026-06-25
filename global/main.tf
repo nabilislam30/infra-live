9d1ddd42cfcd88e3012d40eb7830713c278d5d1e
Nabil Islam <nabil.islam@fimatix.com>
2026-06-25 14:47:18 +0100

    Deploy security baseline v0.3.0

---
+++ global/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "fimatix-devops-starter-tfstate-442847318797"
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+module "security_baseline" {
+  source = "git::https://github.com/nabilislam30/infra-modules.git//security-baseline?ref=v0.3.0"
+}
--- global/versions.tf
+++ global/versions.tf
@@ -3,9 +3,21 @@ terraform {
 
   backend "s3" {
     bucket         = "fimatix-devops-starter-tfstate-442847318797"
-    key            = "dev/vpc/terraform.tfstate"
+    key            = "global/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+  profile = "terraform-devops-starter"
 }
