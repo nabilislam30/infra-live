13cea9804efb15c4811d18144142dba0d30f6523
Nabil Islam <nabil.islam@fimatix.com>
2026-08-20 09:46:03 +0100

    Expose global KMS key output

--- global/outputs.tf
+++ global/outputs.tf
@@ -2,3 +2,11 @@ output "cloudtrail_logs_bucket_arn" {
   description = "ARN of the central S3 bucket used for CloudTrail and VPC Flow Logs."
   value       = module.security_baseline.cloudtrail_logs_bucket_arn
 }
+
+# -----------------------------------------------------------------------------
+# KMS
+# -----------------------------------------------------------------------------
+output "logs_kms_key_arn" {
+  description = "ARN of the customer-managed KMS key from the global security baseline."
+  value       = module.security_baseline.logs_kms_key_arn
+}
